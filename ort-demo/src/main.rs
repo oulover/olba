a0d29536a053058da25a7ece527ec65b89ddc328
PengOu <595466820@qq.com>
2024-06-24 16:47:45 +0800

    init ok  3

--- ort-demo/src/main.rs
+++ ort-demo/src/main.rs
@@ -16,7 +16,29 @@ struct BoundingBox {
     y2: f32,
 }
 
-const DET_10G_URL: &str = "D:\\temp\\aaa\\buffalo_l\\det_10g.onnx";
+fn intersection(box1: &BoundingBox, box2: &BoundingBox) -> f32 {
+    (box1.x2.min(box2.x2) - box1.x1.max(box2.x1)) * (box1.y2.min(box2.y2) - box1.y1.max(box2.y1))
+}
+
+fn union(box1: &BoundingBox, box2: &BoundingBox) -> f32 {
+    ((box1.x2 - box1.x1) * (box1.y2 - box1.y1)) + ((box2.x2 - box2.x1) * (box2.y2 - box2.y1)) - intersection(box1, box2)
+}
+
+// const YOLOV8M_URL: &str = "https://parcel.pyke.io/v2/cdn/assetdelivery/ortrsv2/ex_models/yolov8m.onnx";
+// const YOLOV8M_URL: &str = "D:\\temp\\aaa\\resnet18_imagenet.onnx";
+
+const YOLOV8M_URL: &str = "D:\\temp\\aaa\\yolov8m.onnx";
+#[rustfmt::skip]
+const YOLOV8_CLASS_LABELS: [&str; 80] = [
+    "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
+    "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant",
+    "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard",
+    "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle",
+    "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+    "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch", "potted plant", "bed", "dining table", "toilet",
+    "tv", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator",
+    "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"
+];
 
 #[show_image::main]
 fn main() -> ort::Result<()> {
@@ -25,8 +47,7 @@ fn main() -> ort::Result<()> {
     ort::init()
         .with_execution_providers([CUDAExecutionProvider::default().build()])
         .commit()?;
-
-    let f = "D:\\Temp\\aaa\\baseball.jpg";
+    let f = "D:\\temp\\aaa\\baseball.jpg";
     let original_img = image::open(Path::new(f)).unwrap();
     let (img_width, img_height) = (original_img.width(), original_img.height());
     let img = original_img.resize_exact(640, 640, FilterType::CatmullRom);
@@ -40,29 +61,79 @@ fn main() -> ort::Result<()> {
         input[[0, 2, y, x]] = (b as f32) / 255.;
     }
 
-    let model = Session::builder()?.commit_from_file(DET_10G_URL)?;
+    let model = Session::builder()?.commit_from_file(YOLOV8M_URL)?;
 
-    // Run face detection inference
-    let outputs: SessionOutputs = model.run(inputs!["input.1" => input.view()]?)?;
-    let output = outputs["451"].try_extract_tensor::<f32>()?.t().into_owned();
+    // Run YOLOv8 inference
+    let outputs: SessionOutputs = model.run(inputs!["images" => input.view()]?)?;
+    let output = outputs["output0"].try_extract_tensor::<f32>()?.t().into_owned();
 
     let mut boxes = Vec::new();
+    let output = output.slice(s![.., .., 0]);
+    println!("output---{:?}", output);
     for row in output.axis_iter(Axis(0)) {
+        println!("row---{:?}", row);
         let row: Vec<_> = row.iter().copied().collect();
-        let x1 = row[0] * (img_width as f32);
-        let y1 = row[1] * (img_height as f32);
-        let x2 = row[2] * (img_width as f32);
-        let y2 = row[3] * (img_height as f32);
-        boxes.push(BoundingBox { x1, y1, x2, y2 });
+        println!("row-Vec--{:?}", row);
+        let row22: Vec<_> = row.iter().copied().collect();
+        let row22: Vec<_> = row22
+            .iter()
+            // skip bounding box coordinates
+            .skip(4)
+            .enumerate()
+            .map(|(index, value)| (index, *value)).collect();
+        println!("row22-Vec--{:?}", row22);
+
+        let (class_id, prob) = row
+            .iter()
+            // skip bounding box coordinates
+            .skip(4)
+            .enumerate()
+            .map(|(index, value)| (index, *value))
+            .reduce(|accum, row| if row.1 > accum.1 { row } else { accum })
+            .unwrap();
+        if prob < 0.5 {
+            continue;
+        }
+        let label = YOLOV8_CLASS_LABELS[class_id];
+        let xc = row[0] / 640. * (img_width as f32);
+        let yc = row[1] / 640. * (img_height as f32);
+        let w = row[2] / 640. * (img_width as f32);
+        let h = row[3] / 640. * (img_height as f32);
+        boxes.push((
+            BoundingBox {
+                x1: xc - w / 2.,
+                y1: yc - h / 2.,
+                x2: xc + w / 2.,
+                y2: yc + h / 2.,
+            },
+            label,
+            prob
+        ));
+    }
+
+    boxes.sort_by(|box1, box2| box2.2.total_cmp(&box1.2));
+    let mut result = Vec::new();
+
+    while !boxes.is_empty() {
+        result.push(boxes[0]);
+        boxes = boxes
+            .iter()
+            .filter(|box1| intersection(&boxes[0].0, &box1.0) / union(&boxes[0].0, &box1.0) < 0.7)
+            .copied()
+            .collect();
     }
 
     let mut dt = DrawTarget::new(img_width as _, img_height as _);
 
-    for bbox in boxes {
+    for (bbox, label, _confidence) in result {
         let mut pb = PathBuilder::new();
         pb.rect(bbox.x1, bbox.y1, bbox.x2 - bbox.x1, bbox.y2 - bbox.y1);
         let path = pb.finish();
-        let color = SolidSource { r: 0x00, g: 0xFF, b: 0x00, a: 0x80 };
+        let color = match label {
+            "baseball bat" => SolidSource { r: 0x00, g: 0x10, b: 0x80, a: 0x80 },
+            "baseball glove" => SolidSource { r: 0x20, g: 0x80, b: 0x40, a: 0x80 },
+            _ => SolidSource { r: 0x80, g: 0x10, b: 0x40, a: 0x80 }
+        };
         dt.stroke(
             &path,
             &Source::Solid(color),
@@ -81,18 +152,21 @@ fn main() -> ort::Result<()> {
         .run_function_wait(move |context| -> Result<_, String> {
             let mut window = context
                 .create_window(
-                    "Face Detection",
+                    "ort + YOLOv8",
                     WindowOptions {
                         size: Some([img_width, img_height]),
                         ..WindowOptions::default()
                     },
                 )
                 .map_err(|e| e.to_string())?;
+            // window.set_image("baseball", &original_img.as_image_view().map_err(|e| e.to_string())?);
+
             let tt = original_img.to_rgb8();
             let image_view = ImageView::new(ImageInfo::new(PixelFormat::Bgr8, tt.width(), tt.height()), tt.as_raw());
 
-            window.set_image("face", &image_view);
-            window.set_overlay("face_detection", &overlay.as_image_view().map_err(|e| e.to_string())?, true);
+
+            window.set_image("baseball", &image_view);
+            window.set_overlay("yolo", &overlay.as_image_view().map_err(|e| e.to_string())?, true);
             Ok(window.proxy())
         })
         .unwrap();
