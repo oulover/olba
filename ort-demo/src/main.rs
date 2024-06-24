eee5cba76e150b922b1865fc68aecf61cb3d87df
peng-ou <595466820@qq.com>
2024-06-25 00:36:05 +0800

    iit

--- ort-demo/src/main.rs
+++ ort-demo/src/main.rs
@@ -43,13 +43,44 @@ fn main() -> ort::Result<()> {
 
     let model = Session::builder()?.commit_from_file(DET_10G_URL)?;
 
-    // Run inference
+    // 448 张量（分类分数）和 451 张量（边界框坐标）
+    // （448，451，454）：这是步长为8的一组数据，
+    // 448（12800x1=>1x80x80x2 ）,步长为8，高宽640，640/8=80，每行80个预测框，共80行。通过insightface的源码可以看到，num_anchors = 2，
+    // 每个位置的目标框是两组,正常来说是黑白图两种，既然是同一个位置，那么可以合并一起，意思就是有2张图 ，每张图大小是80x80，有这么多分值。
+    //
+    // 451：bboxs: 1x8x80x80 每一个分数对应的四个点(x1,y1,x2,y2)*注意这个点是距离原点的相对值，
+    // 还是需要计算的,这里1x8  前面1~4 是一个矩形框的点，后面的4~8是另一张图的矩形框坐标点，就是黑白图。
+    //
+    // 454：kps：1x20x80x80 每一个分数对应的五官坐标点（x,y）*注意这个点是距离原点的相对值，
+    // 还是需要计算的，这里1~10 是一组坐标点，另外的10~20是另外一张图的一组坐标点，分开计算就行。
+
     let outputs: SessionOutputs = model.run(inputs!["input.1" => input.view()]?)?;
     let output_451 = outputs["451"].try_extract_tensor::<f32>()?.t().into_owned();
+    let output_448 = outputs["448"].try_extract_tensor::<f32>()?.to_owned();
+
+    println!("output_451  shape{:?}",output_451.shape());
+    println!("output_448  shape{:?}",output_448.shape());
 
     let mut boxes = Vec::new();
-    let output_451 = output_451.slice(s![.., ..,]);
-    for row in output_451.axis_iter(Axis(0)) {
+    // let output_451 = output_451.slice(s![.., ..,]);
+
+
+    let output_451 = output_451.slice(s![0, ..]);  // 选取第一个批次的元素
+
+    // 448（12800x1=>1x80x80x2 ）
+    // output_448  shape[12800, 1]
+    let output_448_reshaped = output_448.into_shape([1,80,80,2]).unwrap(); // 重塑为 (80, 80, 2, 4)
+    println!("output_448_reshaped  shape{:?}",output_448_reshaped.shape());
+
+    // 451：  1x8x80x80 每一个分数对应的四个点(x1,y1,x2,y2)*注意这个点是距离原点的相对值，
+    // output_451  shape[4, 12800]  == 51,200  ==  1x8x80x80
+    let output_451_reshaped = output_451.clone().into_shape([8,80,80]).unwrap();
+    println!("output_451_reshaped  shape{:?}",output_451_reshaped.shape());
+
+
+
+    let x_all = output_448_reshaped.axis_iter(Axis(1));
+    for row in x_all{
         let row: Vec<_> = row.iter().copied().collect();
         let x1 = row[0] / 640. * (img_width as f32);
         let y1 = row[1] / 640. * (img_height as f32);
