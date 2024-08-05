818afc328d67799ac98ae5bc9608183a042ccaf8
peng-ou <595466820@qq.com>
2024-08-05 23:55:28 +0800

    asdassA

--- olbe-ai/src/controller/mod.rs
+++ olbe-ai/src/controller/mod.rs
@@ -1,8 +1,7 @@
 use std::fmt::Display;
-use axum::body::Body;
-use axum::response::{IntoResponse, Response};
-use axum::{Json, Router};
 
+use axum::{Json, Router};
+use axum::response::{IntoResponse, Response};
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use serde::de::DeserializeOwned;
 
@@ -74,8 +73,11 @@ impl<T> R<T> {
     pub fn ok_msg(data: Option<T>, msg: Option<String>) -> R<T> {
         Self::new(Code::Ok200, data, msg)
     }
-    pub fn ok(data: Option<T>) -> R<T> {
+    pub fn ok_opt(data: Option<T>) -> R<T> {
         Self::ok_msg(data, None)
+    }
+    pub fn ok(data:T) -> R<T> {
+        Self::ok_opt(Some(data))
     }
 
     pub fn err_data_msg(data: Option<T>, msg: Option<String>) -> R<T> {
@@ -84,13 +86,22 @@ impl<T> R<T> {
     pub fn err_msg(msg: Option<String>) -> R<()> {
         R::<()>::err_data_msg(None::<()>, msg)
     }
-    pub fn err(msg: Option<String>) -> R<()> {
+    pub fn err_opt(msg: Option<String>) -> R<()> {
         Self::err_msg(msg)
+    }
+    pub fn err(msg: String) -> R<()> {
+        Self::err_opt(Some(msg))
     }
 
     pub fn result<E: Display>(result: Result<T, E>) -> R<T> {
         match result {
-            Ok(data) => { Self::ok(Some(data)) }
+            Ok(data) => { Self::ok(data) }
+            Err(err) => { Self::err_data_msg(None::<T>, Some(err.to_string())) }
+        }
+    }
+    pub fn result_opt<E: Display>(result: Result<Option<T>, E>) -> R<T> {
+        match result {
+            Ok(data) => { Self::ok_opt(data) }
             Err(err) => { Self::err_data_msg(None::<T>, Some(err.to_string())) }
         }
     }
